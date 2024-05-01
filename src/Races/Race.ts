cf68b0e18fe11db267e4de9d0e94d56447affe98
Lucas Giolo <giololucas@gmail.com>
2024-05-01 19:08:14 -0300

    Commit inicial
    Adiciona a classe abstrata Race

---
+++ src/Races/Race.ts
@@ -0,0 +1,11 @@
+abstract class Race {
+  constructor(readonly name: string, readonly dexterity: number) { }
+
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get maxLifePoints(): number;
+}
+
+export default Race;
